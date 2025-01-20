2440b456831832999e6c1ffc0943b62d667e476d
Joe Politz <joe.politz@gmail.com>
2025-01-20 12:20:51 -0800

    simplifying config down to just the web extension

--- src/extension.ts
+++
@@ -1,9 +0,0 @@
-import * as vscode from 'vscode';
-import { PyretParleyProvider } from './pyretParleyEditor';
-import { PyretCPOProvider } from './pyretCPOEditor';
-
-export function activate(context: vscode.ExtensionContext) {
-	// Register our custom editor providers
-	context.subscriptions.push(PyretParleyProvider.register(context));
-	context.subscriptions.push(PyretCPOProvider.register(context));
-}
